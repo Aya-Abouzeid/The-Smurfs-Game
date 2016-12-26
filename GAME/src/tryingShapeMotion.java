9b613120265f2c041817f18196c875732ff96a5c
aya aly saad abouzeid <aya_aly_abouzeid@yahoo.com>
2016-12-26 18:47:26 +0200

    انا مشفتش كده في حياتي والله

--- GAME/src/tryingShapeMotion.java
+++ GAME/src/tryingShapeMotion.java
@@ -18,90 +18,90 @@ import shape.shapePool;
 
 public class tryingShapeMotion extends Application {
 
-    private double screenWidth;
-    private shapePool pool = shapePool.getPoolInstance();
-    private int counter = 0;
-    private ArrayList<shape.Shape> fallingShapes = new ArrayList<shape.Shape>();
-    Image galaxy = new Image("file:galaxy2.png");
-    Image smurfette = new Image("file:Smurfette-original.png");
-    private static ArrayList<Class> loadedShapes;
-    private Player player1 = new Player(null); // --- Send a path
-    private Player player2 = new Player(null);
-    public static void main(String[] args) {
-        loadSahpes();
-        launch(args);
-    }
+	private double screenWidth;
+	private shapePool pool = shapePool.getPoolInstance();
+	private int counter = 0;
+	private ArrayList<shape.Shape> fallingShapes = new ArrayList<shape.Shape>();
+	Image galaxy = new Image("file:galaxy2.png");
+	Image smurfette = new Image("file:Smurfette-original.png");
+	private static ArrayList<Class> loadedShapes;
+	private Player player1 = new Player(null); // --- Send a path
+	private Player player2 = new Player(null);
 
-    private static void loadSahpes() {
-        try {
-            loadedShapes = classLoading.getInstance().getLoadedShapes();
-            shapeFactory.getShapeFactory().setLoadedClasses(loadedShapes);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		loadSahpes();
+		launch(args);
+	}
 
-    @Override
-    public void start(final Stage primaryStage) throws Exception {
-        // TODO Auto-generated method stub
-        primaryStage.setTitle("Game");
-        primaryStage.setFullScreen(true);
+	private static void loadSahpes() {
+		try {
+			loadedShapes = classLoading.getInstance().getLoadedShapes();
+			shapeFactory.getShapeFactory().setLoadedClasses(loadedShapes);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-        ImageView img = new ImageView(galaxy);
-        img.setPreserveRatio(true);
-        img.fitWidthProperty().bind(primaryStage.widthProperty());
+	@Override
+	public void start(final Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		primaryStage.setTitle("Game");
+		primaryStage.setFullScreen(true);
 
-        final Group root = new Group();
+		ImageView img = new ImageView(galaxy);
+		img.setPreserveRatio(true);
+		img.fitWidthProperty().bind(primaryStage.widthProperty());
 
-        Scene scene = new Scene(root, primaryStage.getWidth(), primaryStage.getHeight(), Color.WHITE);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+		final Group root = new Group();
 
-        root.getChildren().add(img);
+		Scene scene = new Scene(root, primaryStage.getWidth(), primaryStage.getHeight(), Color.WHITE);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 
-        Canvas canvas = new Canvas(primaryStage.getWidth(), primaryStage.getHeight());
-        root.getChildren().add(canvas);
-        primaryStage.show();
-        fallingShapes.add(pool.borrowObject(primaryStage.getWidth()));
-        final GraphicsContext gc = canvas.getGraphicsContext2D();
-        // gc.drawImage(galaxy, 0, 0);
-        gc.drawImage(smurfette, 0, 0);
+		root.getChildren().add(img);
 
-        gc.setStroke(Color.BLACK);
-        new AnimationTimer() {
-            @Override
-            public void handle(long currentNanoTime) {
-                draw(gc, primaryStage.getHeight(), primaryStage.getWidth());
-            }
-        }.start();
-        primaryStage.setScene(scene);
-        primaryStage.show();
+		Canvas canvas = new Canvas(primaryStage.getWidth(), primaryStage.getHeight());
+		root.getChildren().add(canvas);
+		primaryStage.show();
+		fallingShapes.add(pool.borrowObject(primaryStage.getWidth()));
+		final GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.drawImage(smurfette, 0, 0);
 
-    }
+		gc.setStroke(Color.BLACK);
+		new AnimationTimer() {
+			@Override
+			public void handle(long currentNanoTime) {
+				draw(gc, primaryStage.getHeight(), primaryStage.getWidth());
+			}
+		}.start();
+		primaryStage.setScene(scene);
+		primaryStage.show();
 
-    public void draw(GraphicsContext gc, double height, double width) {
+	}
 
-        gc.clearRect(0, 0, width, height);
-        gc.drawImage(smurfette, 0, 0);
-        counter++;
-        if (counter % 15 == 0)
-            fallingShapes.add(pool.borrowObject(width));
-        for (int i = 0; i < fallingShapes.size(); i++) {
-            if (fallingShapes.get(i).getY() >= height) {
-                pool.returnObject(fallingShapes.get(i));
-                fallingShapes.remove(i--);
-            }else if(fallingShapes.get(i).getX() == player1.getX() || fallingShapes.get(i).getX() == player2.getX()){
-                catchDetection(fallingShapes.get(i));
-            }
-            else {
-                fallingShapes.get(i).setY(fallingShapes.get(i).getY() + 4.0);
-                fallingShapes.get(i).drawShape(gc);
-            }
-        }
-    }
+	public void draw(GraphicsContext gc, double height, double width) {
 
-    private void catchDetection(Shape shape) {
+		gc.clearRect(0, 0, width, height);
+		gc.drawImage(smurfette, 0, 0);
+		counter++;
+		if (counter % 15 == 0) {
+			fallingShapes.add(pool.borrowObject(width));
+			counter = 0;
+		}
+		for (int i = 0; i < fallingShapes.size(); i++) {
+			if (fallingShapes.get(i).getY() >= height) {
+				pool.returnObject(fallingShapes.get(i));
+				fallingShapes.remove(i--);
+			} else if (fallingShapes.get(i).getX() == player1.getX() || fallingShapes.get(i).getX() == player2.getX()) {
+				catchDetection(fallingShapes.get(i));
+			} else {
+				fallingShapes.get(i).setY(fallingShapes.get(i).getY() + 4.0);
+				fallingShapes.get(i).drawShape(gc);
+			}
+		}
+	}
 
+	private void catchDetection(Shape shape) {
 
-    }
+	}
 }
